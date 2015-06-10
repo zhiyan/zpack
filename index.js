326ad7625c9a79cfdf472968157d7addbdf813db
zhiyan <wangzhiyan@rong360.com>
2015-06-10 15:59:23 +0800

    comments

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+// expose zpack
+module.exports = {
+    run : run
+}
+
+/**
+ * zpack命令入口
+ * @param  {String} cmd 命令名称
+ */
 function run( cmd ){
     var command = require( "./lib/" + cmd + ".js" );
     command.run();
-}
-
-module.exports = {
-    run : run
 }
